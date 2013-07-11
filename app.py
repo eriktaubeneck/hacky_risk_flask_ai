91954648ca04e344ecdf4eef1a4d676ba9e98c8d
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-07-11 01:45:32 -0400

    added choose country fuction

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import sys
-from flask import Flask
+from flask import Flask, request
 from risk.models import *
+import json
+import random
 
-board, card_lookup = models.import_board_data('./risk/board_graph.json')
+board = import_board_data('./risk/board_graph.json')
 
 app = Flask(__name__)
 
@@ -16,6 +18,18 @@ def not_turn():
     print 'got board'
     return ''
 
+@app.route('/turn', methods=['POST'])
+def turn():
+    r = json.loads(request.form['risk'])
+    available_actions = r['you']['available_actions']
+    if "choose_country" in available_actions:
+        countries = r['game']['countries']
+        unoccupied = [c for c in countries if not countries[c]['owner']]
+        country_choice = random.choice(unoccupied)
+        response = {"action":"choose_country", "data":country_choice}
+        print "choose: %s" % country_choice
+        return json.dumps(response)
+    return ''
 
 if __name__ == '__main__':
     port = int(sys.argv[1])
